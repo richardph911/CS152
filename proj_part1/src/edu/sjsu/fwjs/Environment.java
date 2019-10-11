b0878f5bec10bc6996d7bd0cc21b0fec6da8f1ac
khoaLib <phamkhoa996@gmail.com>
2019-10-11 16:20:45 -0700

    finish Binop

--- proj_part1/src/edu/sjsu/fwjs/Environment.java
+++ proj_part1/src/edu/sjsu/fwjs/Environment.java
@@ -4,94 +4,97 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Environment {
-    private Map<String,Value> env = new HashMap<String,Value>();
-    private Environment outerEnv;
+	private Map<String, Value> env = new HashMap<String, Value>();
+	private Environment outerEnv;
 
-    /**
-     * Constructor for global environment
-     */
-    public Environment() {}
+	/**
+	 * Constructor for global environment
+	 */
+	public Environment() {
+	}
 
-    /**
-     * Constructor for local environment of a function
-     */
-    public Environment(Environment outerEnv) {
-        this.outerEnv = outerEnv;
-    }
+	/**
+	 * Constructor for local environment of a function
+	 */
+	public Environment(Environment outerEnv) {
+		this.outerEnv = outerEnv;
+	}
 
-    /**
-     * Handles the logic of resolving a variable.
-     * If the variable name is in the current scope, it is returned.
-     * Otherwise, search for the variable in the outer scope.
-     * If we are at the outermost scope (AKA the global scope)
-     * null is returned (similar to how JS returns undefined.
-     */
-    public Value resolveVar(String varName) {
+	/**
+	 * Handles the logic of resolving a variable. If the variable name is in the
+	 * current scope, it is returned. Otherwise, search for the variable in the
+	 * outer scope. If we are at the outermost scope (AKA the global scope) null is
+	 * returned (similar to how JS returns undefined.
+	 */
+	public Value resolveVar(String varName) {
 
-        // YOUR CODE HERE
+		// YOUR CODE HERE
 
-        // look for var at current scope
-        if (env.containsKey(varName))
-            return env.get(varName);
+		// look for var at current scope
+		if (env.containsKey(varName))
+			return env.get(varName);
 
-        Environment tempOuterEnv = this.outerEnv;
-        while (tempOuterEnv != null) {
-             // look for var at the outer scope
-            if (tempOuterEnv.env.containsKey(varName))
-                return tempOuterEnv.env.get(varName);
-            
-            tempOuterEnv = tempOuterEnv.outerEnv;
-        }
-        
-        return null;
-    }
+		Environment tempOuterEnv = this.outerEnv;
+		while (tempOuterEnv != null) {
+			// look for var at the outer scope
+			if (tempOuterEnv.env.containsKey(varName))
+				return tempOuterEnv.env.get(varName);
 
-    /**
-     * Used for updating existing variables.
-     * If a variable has not been defined previously in the current scope,
-     * or any of the function's outer scopes, the var is stored in the global scope.
-     */
-    public void updateVar(String key, Value v) {
+			tempOuterEnv = tempOuterEnv.outerEnv;
+		}
 
-        // YOUR CODE HERE
+		return new NullVal();
+	}
 
-        if (env.containsKey(key)) {
-            env.put(key, v);
-            return;
-        }
+	/**
+	 * Used for updating existing variables. If a variable has not been defined
+	 * previously in the current scope, or any of the function's outer scopes, the
+	 * var is stored in the global scope.
+	 */
+	public void updateVar(String key, Value v) {
 
-        Environment tempOuterEnv = outerEnv;
-        while (tempOuterEnv != null) {
-            // look for var at the outer scope
-            if (tempOuterEnv.env.containsKey(key)) {
-                tempOuterEnv.env.put(key, v);
-                return;
-            }
-            
-            // create new var at the global scope when var is not define in any scope
-            if (tempOuterEnv.outerEnv == null) {
-                tempOuterEnv.env.put(key, v);
-                return;
-            }
+		// YOUR CODE HERE
 
-            tempOuterEnv = tempOuterEnv.outerEnv;
-        }
-        
-    }
+		if (env.containsKey(key)) {
+			env.put(key, v);
+			return;
+		}
 
-    /**
-     * Creates a new variable in the local scope.
-     * If the variable has been defined in the current scope previously,
-     * a RuntimeException is thrown.
-     */
-    public void createVar(String key, Value v) {
+		if (outerEnv == null) {
+			createVar(key, v);
+		}
 
-        // YOUR CODE HERE
-        
-        if(env.containsKey(key))
-            throw new RuntimeException("Error: Variable already exist in the scope!");
-        else
-            env.put(key, v);
-    }
+		Environment tempOuterEnv = outerEnv;
+		while (tempOuterEnv != null) {
+			// look for var at the outer scope
+			if (tempOuterEnv.env.containsKey(key)) {
+				tempOuterEnv.env.put(key, v);
+				return;
+			}
+
+			// create new var at the global scope when var is not define in any scope
+			if (tempOuterEnv.outerEnv == null) {
+				tempOuterEnv.env.put(key, v);
+				return;
+			}
+
+			tempOuterEnv = tempOuterEnv.outerEnv;
+		}
+
+	}
+
+	/**
+	 * Creates a new variable in the local scope. If the variable has been defined
+	 * in the current scope previously, a RuntimeException is thrown.
+	 */
+	public void createVar(String key, Value v) {
+
+		// YOUR CODE HERE
+
+		if (env.containsKey(key))
+			throw new RuntimeException("Error: Variable already exist in the scope!");
+		else
+			env.put(key, v);
+	}
 
 }
--- proj_part1/src/edu/sjsu/fwjs/Expression.java
+++ proj_part1/src/edu/sjsu/fwjs/Expression.java
@@ -7,10 +7,10 @@ import java.util.List;
  * FWJS expressions.
  */
 public interface Expression {
-    /**
-     * Evaluate the expression in the context of the specified environment.
-     */
-    public Value evaluate(Environment env);
+	/**
+	 * Evaluate the expression in the context of the specified environment.
+	 */
+	public Value evaluate(Environment env);
 }
 
 // NOTE: Using package access so that all implementations of Expression
@@ -20,213 +20,232 @@ public interface Expression {
  * FWJS constants.
  */
 class ValueExpr implements Expression {
-    private Value val;
-    public ValueExpr(Value v) {
-        this.val = v;
-    }
-    public Value evaluate(Environment env) {
-        return this.val;
-    }
+	private Value val;
+
+	public ValueExpr(Value v) {
+		this.val = v;
+	}
+
+	public Value evaluate(Environment env) {
+		return this.val;
+	}
 }
 
 /**
  * Expressions that are a FWJS variable.
  */
 class VarExpr implements Expression {
-    private String varName;
-    public VarExpr(String varName) {
-        this.varName = varName;
-    }
-    public Value evaluate(Environment env) {
-        return env.resolveVar(varName);
-    }
+	private String varName;
+
+	public VarExpr(String varName) {
+		this.varName = varName;
+	}
+
+	public Value evaluate(Environment env) {
+		return env.resolveVar(varName);
+	}
 }
 
 /**
  * A print expression.
  */
 class PrintExpr implements Expression {
-    private Expression exp;
-    public PrintExpr(Expression exp) {
-        this.exp = exp;
-    }
-    public Value evaluate(Environment env) {
-        Value v = exp.evaluate(env);
-        System.out.println(v.toString());
-        return v;
-    }
+	private Expression exp;
+
+	public PrintExpr(Expression exp) {
+		this.exp = exp;
+	}
+
+	public Value evaluate(Environment env) {
+		Value v = exp.evaluate(env);
+		System.out.println(v.toString());
+		return v;
+	}
 }
+
 /**
- * Binary operators (+, -, *, etc).
- * Currently only numbers are supported.
+ * Binary operators (+, -, *, etc). Currently only numbers are supported.
  */
 class BinOpExpr implements Expression {
-    private Op op;
-    private Expression e1;
-    private Expression e2;
-    public BinOpExpr(Op op, Expression e1, Expression e2) {
-        this.op = op;
-        this.e1 = e1;
-        this.e2 = e2;
-    }
+	private Op op;
+	private Expression e1;
+	private Expression e2;
 
-    @SuppressWarnings("incomplete-switch")
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        Value v1 = e1.evaluate(env);
-        Value v2 = e2.evaluate(env);
-        IntVal r;
-        switch(op) {
-            case ADD:
-                r = new IntVal(Integer.parseInt(v1.toString()) + Integer.parseInt(v2.toString()));
-                return (Value) r;
+	public BinOpExpr(Op op, Expression e1, Expression e2) {
+		this.op = op;
+		this.e1 = e1;
+		this.e2 = e2;
+	}
 
-            case SUBTRACT:
-                r = new IntVal(Integer.parseInt(v1.toString()) - Integer.parseInt(v2.toString()));
-                return (Value) r;
+	@SuppressWarnings("incomplete-switch")
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		Value v1 = e1.evaluate(env);
+		Value v2 = e2.evaluate(env);
+		IntVal r;
+		switch (op) {
+		case ADD:
+			r = new IntVal(Integer.parseInt(v1.toString()) + Integer.parseInt(v2.toString()));
+			return (Value) r;
 
-            case MULTIPLY:
-                r = new IntVal(Integer.parseInt(v1.toString()) * Integer.parseInt(v2.toString()));
-                return (Value) r;
+		case SUBTRACT:
+			r = new IntVal(Integer.parseInt(v1.toString()) - Integer.parseInt(v2.toString()));
+			return (Value) r;
 
-            case DIVIDE:
-                r = new IntVal(Integer.parseInt(v1.toString()) / Integer.parseInt(v2.toString()));
-                return (Value) r;
+		case MULTIPLY:
+			r = new IntVal(Integer.parseInt(v1.toString()) * Integer.parseInt(v2.toString()));
+			return (Value) r;
 
-            case EQ:
-                return (Value) new BoolVal(v1.equals(v2));
-            
+		case DIVIDE:
+			r = new IntVal(Integer.parseInt(v1.toString()) / Integer.parseInt(v2.toString()));
+			return (Value) r;
 
-            case GE:
+		case EQ:
+			return new BoolVal(v1.equals(v2));
 
-            case GT:
+		case GE:
+			return new BoolVal(Integer.parseInt(v1.toString()) >= Integer.parseInt(v2.toString()));
 
-            case LE:
+		case GT:
+			return new BoolVal(Integer.parseInt(v1.toString()) > Integer.parseInt(v2.toString()));
+		case LE:
+			return new BoolVal(Integer.parseInt(v1.toString()) <= Integer.parseInt(v2.toString()));
+		case LT:
+			return new BoolVal(Integer.parseInt(v1.toString()) < Integer.parseInt(v2.toString()));
+		case MOD:
+			r = new IntVal(Integer.parseInt(v1.toString()) % Integer.parseInt(v2.toString()));
+			return (Value) r;
+		}
+		return v1;
 
-            case LT:
-
-            case MOD:
-        }
-        return v1;
-
-        
-    }
+	}
 }
 
 /**
- * If-then-else expressions.
- * Unlike JS, if expressions return a value.
+ * If-then-else expressions. Unlike JS, if expressions return a value.
  */
 class IfExpr implements Expression {
-    private Expression cond;
-    private Expression thn;
-    private Expression els;
-    public IfExpr(Expression cond, Expression thn, Expression els) {
-        this.cond = cond;
-        this.thn = thn;
-        this.els = els;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private Expression cond;
+	private Expression thn;
+	private Expression els;
+
+	public IfExpr(Expression cond, Expression thn, Expression els) {
+		this.cond = cond;
+		this.thn = thn;
+		this.els = els;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
  * While statements (treated as expressions in FWJS, unlike JS).
  */
 class WhileExpr implements Expression {
-    private Expression cond;
-    private Expression body;
-    public WhileExpr(Expression cond, Expression body) {
-        this.cond = cond;
-        this.body = body;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private Expression cond;
+	private Expression body;
+
+	public WhileExpr(Expression cond, Expression body) {
+		this.cond = cond;
+		this.body = body;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
  * Sequence expressions (i.e. 2 back-to-back expressions).
  */
 class SeqExpr implements Expression {
-    private Expression e1;
-    private Expression e2;
-    public SeqExpr(Expression e1, Expression e2) {
-        this.e1 = e1;
-        this.e2 = e2;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private Expression e1;
+	private Expression e2;
+
+	public SeqExpr(Expression e1, Expression e2) {
+		this.e1 = e1;
+		this.e2 = e2;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
  * Declaring a variable in the local scope.
  */
 class VarDeclExpr implements Expression {
-    private String varName;
-    private Expression exp;
-    public VarDeclExpr(String varName, Expression exp) {
-        this.varName = varName;
-        this.exp = exp;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private String varName;
+	private Expression exp;
+
+	public VarDeclExpr(String varName, Expression exp) {
+		this.varName = varName;
+		this.exp = exp;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
- * Updating an existing variable.
- * If the variable is not set already, it is added
- * to the global scope.
+ * Updating an existing variable. If the variable is not set already, it is
+ * added to the global scope.
  */
 class AssignExpr implements Expression {
-    private String varName;
-    private Expression e;
-    public AssignExpr(String varName, Expression e) {
-        this.varName = varName;
-        this.e = e;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private String varName;
+	private Expression e;
+
+	public AssignExpr(String varName, Expression e) {
+		this.varName = varName;
+		this.e = e;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
  * A function declaration, which evaluates to a closure.
  */
 class FunctionDeclExpr implements Expression {
-    private List<String> params;
-    private Expression body;
-    public FunctionDeclExpr(List<String> params, Expression body) {
-        this.params = params;
-        this.body = body;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
+	private List<String> params;
+	private Expression body;
+
+	public FunctionDeclExpr(List<String> params, Expression body) {
+		this.params = params;
+		this.body = body;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
 }
 
 /**
  * Function application.
  */
 class FunctionAppExpr implements Expression {
-    private Expression f;
-    private List<Expression> args;
-    public FunctionAppExpr(Expression f, List<Expression> args) {
-        this.f = f;
-        this.args = args;
-    }
-    public Value evaluate(Environment env) {
-        // YOUR CODE HERE
-        return null;
-    }
-}
+	private Expression f;
+	private List<Expression> args;
 
+	public FunctionAppExpr(Expression f, List<Expression> args) {
+		this.f = f;
+		this.args = args;
+	}
+
+	public Value evaluate(Environment env) {
+		// YOUR CODE HERE
+		return null;
+	}
+}
